7975826e2aa2707a75d208199327b20fed4f10fe
Muhasin <mahmudmuhasin@gmail.com>
2020-10-20 15:48:43 +0200

    initial commit

---
+++ muhasin/index.js
@@ -0,0 +1,8 @@
+const colors = ["red", 9999, "green", "blue", 777, "yellow", "black", "purple", "brown"];
+
+
+
+
+
+const randomColor = colors[Math.floor(Math.random() * colors.length)];
+console.log("randomColor ==> ", randomColor);
