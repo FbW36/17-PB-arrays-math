0fe2ac3df87ceea452f46e130b21d94088a7b3b5
Zarzarius <zarzarius@gmail.com>
2020-10-20 14:29:53 +0200

    exercise done

---
+++ Azael/index.js
@@ -0,0 +1,24 @@
+// # 18-PB-arrays-math
+
+const colors = [
+  "red",
+  9999,
+  "green",
+  "blue",
+  777,
+  "yellow",
+  "black",
+  "purple",
+  "brown",
+];
+
+// *** Task // with Math.random() ==> get random item from colors array, if the item is number set it to ''
+
+let colorsLength = colors.length;
+
+console.log(colorsLength);
+
+let random = Math.floor(Math.random() * colorsLength);
+let randomColors = colors[random];
+
+console.log(randomColors);
