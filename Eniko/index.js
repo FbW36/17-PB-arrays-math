0033bab5fb648ce2ffb59ee1c5c0dd55fe730c6f
Enikő Veszelka <encsooo@gmail.com>
2020-10-20 15:02:38 +0200

    i tried

---
+++ Eniko/index.js
@@ -0,0 +1,15 @@
+const colors = ["red", 9999, "green", "blue",777, "yellow", "black", "purple", "brown"];
+
+// *** Task // with Math.random() ==> get random item from colors array, if the item is number set it to ''
+
+// 🚀 Enjoy the challenges
+
+let randomItem = colors[Math.floor(Math.random() * 10)];
+console.log(randomItem);
+if (colors == Number) {
+    randomItem = "";
+}
+
+console.log(randomItem);
+
+//not sure if this is correct
