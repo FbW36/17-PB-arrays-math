696ca4fa18c56c929ef7c49c2011b21421f7d9ae
Chrissi <cmette.online@googlemail.com>
2020-10-20 14:39:30 +0200

    add solution

---
+++ christin/index.js
@@ -0,0 +1,21 @@
+const colors = [
+  "red",
+  9999,
+  "green",
+  "blue",
+  777,
+  "yellow",
+  "black",
+  "purple",
+  "brown",
+];
+
+colors[1] = [""];
+colors[4] = [""];
+
+console.log(
+  "get random item:",
+  colors[Math.floor(Math.random() * colors.length)]
+);
+
+// *** Task // with Math.random() ==> get random item from colors array, if the item is number set it to ''
