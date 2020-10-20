c019b61f972e35c45207431688183ef3731e0e25
Joyce <puch1996@gmail.com>
2020-10-20 15:00:57 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,14 @@
+const colors = [
+  "red",
+  9999,
+  "green",
+  "blue",
+  777,
+  "yellow",
+  "black",
+  "purple",
+  "brown",
+];
+const random = colors[Math.floor(Math.random() * colors.length)];
+const pickColor = isNaN(random) === true ? random : "";
+console.log(pickColor);
