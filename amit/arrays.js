a6e1ec7e00a8669bbe3cfc90e28c1e8463262eee
Amit833 <amitmajumder833@gmail.com>
2020-10-20 23:06:18 +0200

    Amit,2nd commit

--- amit/arrays.js
+++ amit/arrays.js
@@ -13,5 +13,8 @@ const colors = [
 const colors2 = colors[Math.floor(Math.random() * colors.length)];
 console.log(colors2);
 
-let ans = isNaN(colors2) === true ? colors2 : "";
+let ans = typeof colors2 === "number" ? "" : colors2;
 console.log(ans);
+
+// or let ans = isNaN(colors2) === true ? colors2 : "";
+// console.log(ans);
