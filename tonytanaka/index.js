305678a98d10cb252298bf4755d3840278a14ade
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-20 14:45:54 +0200

    finished exercise

---
+++ tonytanaka/index.js
@@ -0,0 +1,19 @@
+const colors = [
+  "red",
+  9999,
+  "green",
+  "blue",
+  777,
+  "yellow",
+  "black",
+  "purple",
+  "brown",
+];
+
+randomIndex = Math.floor(Math.random() * colors.length);
+console.log("Random Index is ==>", randomIndex);
+console.log("Its color is ==>", colors[randomIndex]);
+let whichType =
+  typeof colors[randomIndex] == "string"
+    ? console.log("It is a string =", colors[randomIndex])
+    : console.log("It is a number =", colors[""]);
