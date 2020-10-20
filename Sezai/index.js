c5f80e38efbc7e3791e6baa247d05d301bc73401
Sezai Keskin <sezai.keskin@me.com>
2020-10-20 15:00:33 +0200

    added changes

--- Sezai/index.js
+++ Sezai/index.js
@@ -8,3 +8,9 @@ colors[4] = "";
 let colorPicker = colors[Math.floor(Math.random() * colors.length)];
 console.log("Hallo ==> ", colorPicker);
 
+// ciamacs Solution
+
+const colors2 = ["red", 9999, "green", "blue",777, "yellow", "black", "purple", "brown"];
+let colorPicker2 = Math.floor(Math.random() * colors2.length);
+let filtered = typeof colors2[colorPicker2] === "number" ? "" : colors2[colorPicker2];
+console.log(filtered);
