cd1386f0ffa7e236daec2fcf87cf1a036f0a9fa7
Muhasin <mahmudmuhasin@gmail.com>
2020-10-22 17:11:03 +0200

    rewrite code

--- muhasin/index.js
+++ muhasin/index.js
@@ -1,8 +1,6 @@
 const colors = ["red", 9999, "green", "blue", 777, "yellow", "black", "purple", "brown"];
 
-
-
-
-
 const randomColor = colors[Math.floor(Math.random() * colors.length)];
+isNaN(randomColor) ? randomColor : randomColor = "";
+
 console.log("randomColor ==> ", randomColor);
