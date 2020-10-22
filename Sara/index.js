740dd63b4fa6703dad23bd98fb49398f3bc9e93a
Sara <you@example.com>
2020-10-22 15:45:03 +0200

    finished

--- Sara/index.js
+++ Sara/index.js
@@ -1,18 +1,15 @@
 // with Math.random() ==> get random item from colors array, if the item is number set it to '' 
 const colors = ["red", 9999, "green", "blue", 777, "yellow", "black", "purple", "brown"] ; // 
-// turn number into string number
-colors[1] = colors[1].toString() ; // '999'
-colors[4] = colors[4].toString() ; // '777'
-console.log('colors ==>', colors) ; // 
 
 // length of array colors
 console.log('length of colors ==>', colors.length) ; // 9
 
 let randomItem = colors[Math.floor(Math.random() * colors.length)] ;
-console.log('randomItem ==>', randomItem) ; // random color 
+console.log('randomItem ==>', randomItem) ; // random Item
 
+const pickColor = isNaN(randomItem) === true ? randomItem : "";
+console.log('pickColor ==>', pickColor); // numbers not included
 
- 
 
 
 
