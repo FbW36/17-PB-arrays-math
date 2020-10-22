2c7705b134139ab860c53015894e87392b2047b4
Enikő Veszelka <encsooo@gmail.com>
2020-10-22 13:19:13 +0200

    bug fixes

--- Eniko/index.js
+++ Eniko/index.js
@@ -1,15 +1,19 @@
-const colors = ["red", 9999, "green", "blue",777, "yellow", "black", "purple", "brown"];
 
 // *** Task // with Math.random() ==> get random item from colors array, if the item is number set it to ''
 
 // 🚀 Enjoy the challenges
-console.log('length of colors ==>', colors.length) ; // 9
-let randomItem = colors[Math.floor(Math.random() * 9)];
-console.log(randomItem);
-if (colors == Number) {
-    randomItem = "";
-}
 
-console.log(randomItem);
-
-//not sure if this is correct
+const colors = [
+    "red",
+    9999,
+    "green",
+    "blue",
+    777,
+    "yellow",
+    "black",
+    "purple",
+    "brown",
+    ];
+    const random = colors[Math.floor(Math.random() * colors.length)];
+    const pickColor = isNaN(random) === true ? random : "";
+    console.log(pickColor);
